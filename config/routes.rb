3b2f56b46dd8b0bdf56ecdb4e44b56d22910e830
tomimatsu0224yuki <tomimatsu0224yuki@gmail.com>
2020-11-11 08:51:40 +0900

    新規登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "items#index"
+  resources :items, only: :index
+
+  resources :users, only: :show
 end
