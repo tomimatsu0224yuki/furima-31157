cbacf6425851e89f9d4c098c49a5659247df158a
tomimatsu0224yuki <tomimatsu0224yuki@gmail.com>
2020-11-12 15:23:29 +0900

    バリデーションの正規表現の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/}
   validates :birth_date, presence: true
  
   has_many :items
