1498275b353cb52ee0bf3ea935460b4fe6a6f244
tomimatsu0224yuki <tomimatsu0224yuki@gmail.com>
2020-11-19 21:31:48 +0900

    修正３

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-with_options presence:true do
-  validates :image
-  validates :item_name,length: { maximum: 40 }
-  validates :item_discription, length: { maximum: 1000 }
-  
-  with_options numericality: {other_than: 1} do
-    validates :category_id 
-    validates :condition_id
-    validates :shipping_charge_id
-    validates :processing_time_id
-  end
+  with_options presence: true do
+    validates :image
+    validates :item_name, length: { maximum: 40 }
+    validates :item_discription, length: { maximum: 1000 }
 
-  validates :prefecture_id, numericality: { other_than: 0 }
-  validates :price, format: { with: /\d+/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-end
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_charge_id
+      validates :processing_time_id
+    end
+
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :price, format: { with: /\d+/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
 
   has_one_attached :image
   belongs_to :user
