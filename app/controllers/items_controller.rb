2094f833d8858358d54252741bb4c5e90fb50aaf
tomimatsu0224yuki <tomimatsu0224yuki@gmail.com>
2020-11-20 20:51:45 +0900

    ログアウト状態のユーザーのリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -23,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def update
